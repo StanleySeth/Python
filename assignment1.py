18673527b8437d11fcf9caf13f4b04e5f3a93b16
Stanley Seth <stanleyseth69@gmail.com>
2026-02-06 10:22:27 +0300

    Updated python file

---
+++ assignment1.py
@@ -0,0 +1,16 @@
+
+
+# Assignment
+# 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI = (weight) / (height square)
+We = 78
+H = 1.75
+BMI = (We // H **2)
+print("The BMI is:",BMI)
+# 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25m
+L = 48
+Wi = 25
+P = ((L * 2) + (Wi * 2))
+print("The perimeter is:" ,P)
+A = (L * Wi)
+print("The area is:", A)
+# Research on python list, tuple and Dictionary Data type
---
+++ lesson1d.py
@@ -0,0 +1,6 @@
+#Create a python program that is able to calculate the simple interest given the principle as 50000, rate as 5% and time is 8 years. SI = (p * R * t)/100
+principle = 50000
+rate = 5
+time = 8
+SI = (principle * rate * time)/100
+print(SI)
